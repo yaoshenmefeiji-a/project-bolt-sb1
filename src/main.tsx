51e4380b03aadbaeacde01bf2543f117fc28083f
Jack Lee <jack@Jack.local>
2024-12-29 00:34:30 +0800

    fix: add basename to BrowserRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/project-bolt-sb1">
       <App />
     </BrowserRouter>
   </StrictMode>
